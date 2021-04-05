1a5093cd6b9afade53a7de63e717129668b0a1c2
hhcol <2838967868@qq.com>
2021-04-05 20:35:29 +0800

    fix

--- src/components/Charts/index.vue
+++ src/components/Charts/index.vue
@@ -11,7 +11,7 @@
           最近一月
         </a-select-option>
         <a-select-option value="2">
-          最近三个月
+          最近一年
         </a-select-option>
       </a-select>
     </div>
@@ -87,43 +87,43 @@ export default {
         this.getReadTimesTrend()
       }
     }
-  },
+  }
 }
 </script>
 
 <style lang="less" scoped>
 #container {
-  height: 400px;
+    height: 400px;
 }
 
 .highcharts-data-table table {
-  font-family: Verdana, sans-serif;
-  border-collapse: collapse;
-  border: 1px solid #ebebeb;
-  margin: 10px auto;
-  text-align: center;
-  width: 100%;
-  max-width: 500px;
+    font-family: Verdana, sans-serif;
+    border-collapse: collapse;
+    border: 1px solid #ebebeb;
+    margin: 10px auto;
+    text-align: center;
+    width: 100%;
+    max-width: 500px;
 }
 .highcharts-data-table caption {
-  padding: 1em 0;
-  font-size: 1.2em;
-  color: #555;
+    padding: 1em 0;
+    font-size: 1.2em;
+    color: #555;
 }
 .highcharts-data-table th {
-  font-weight: 600;
-  padding: 0.5em;
+    font-weight: 600;
+    padding: 0.5em;
 }
 .highcharts-data-table td,
 .highcharts-data-table th,
 .highcharts-data-table caption {
-  padding: 0.5em;
+    padding: 0.5em;
 }
 .highcharts-data-table thead tr,
 .highcharts-data-table tr:nth-child(even) {
-  background: #f8f8f8;
+    background: #f8f8f8;
 }
 .highcharts-data-table tr:hover {
-  background: #f1f7ff;
+    background: #f1f7ff;
 }
 </style>
